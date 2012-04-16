7a915e1cca4815adcc13afafeb3cf6ab15ffee20
Kartik Singhal <kartiksinghal@gmail.com>
2012-04-16 09:50:23 +0530

    added test files for modif program, minor input test change in modif.c 	new file:   asdfg.txt 	new file:   def.txt 	modified:   modif.c

--- nachos-3.4/code/test/modif.c
+++ nachos-3.4/code/test/modif.c
@@ -1,7 +1,7 @@
 void main()
 {
 	Print("=== Modif program initiated ===\n");
-	MyCopy("asdfg.txt", "def.txt", 40, 5); //	MyCopy(file1, file2, pos1, pos2);
+	MyCopy("asdfg.txt", "def.txt", 40, 50); //	MyCopy(file1, file2, pos1, pos2);
 	Print("=== Modif program terminated ===\n");
 	Halt();	// Optional. Just print stats
 }
